cf4a4e46421fc24da35cbaf816c0b1cacb9185ed
pedro.dulce <pedro.dulce@gmail.com>
2024-05-14 18:42:57 +0200

    partial done

--- src/main/java/com/arch/mfc/application/service/CustomerOrderService.java
+++ src/main/java/com/arch/mfc/application/service/CustomerOrderService.java
@@ -1,6 +1,6 @@
 package com.arch.mfc.application.service;
 
-import com.arch.mfc.application.repository.CustomerOrderRepository;
+import com.arch.mfc.application.domain.CustomerOrder;
 import com.arch.mfc.infra.service.BaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomerOrderService extends BaseService {
     @Autowired
-    CustomerOrderRepository entityRepository;
+    JpaRepository<CustomerOrder, Long> entityRepository;
 
     @Override
     public JpaRepository getJPaRepository() {
