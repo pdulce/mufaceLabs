97e832b84571543eaad30066d87972efb0365160
pedro.dulce <pedro.dulce@gmail.com>
2024-05-14 18:45:24 +0200

    partial done

--- src/main/java/com/arch/mfc/application/repository/CustomerRepository.java
+++ src/main/java/com/arch/mfc/application/repository/CustomerRepository.java
@@ -6,8 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+/**
+ * Esta clase la crearemos solo cuando necesite la capa de negocio de la aplicación nuevas operaciones
+ * no disponibles desde la arquitectura, por ejemplo, consultas HQL persionalizas con JOINS etc,
+ * consultas filtrando por uno o varios campos específicos de la entidad que maneja este Repository
+ */
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-    public List<Customer> findByName(String name);
+    List<Customer> findByName(String name);
 
 }
