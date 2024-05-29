02c4cabb3241144dcefc7b4ea290e7dca74c6eea
pedro.dulce <pedro.dulce@gmail.com>
2024-05-29 20:43:53 +0200

    partial done

--- src/main/java/com/arch/mfc/application/service/query/CustomerQueryService.java
+++ src/main/java/com/arch/mfc/application/service/query/CustomerQueryService.java
@@ -2,19 +2,18 @@ package com.arch.mfc.application.service.query;
 
 import com.arch.mfc.application.domain.document.CustomerDocument;
 import com.arch.mfc.application.repository.document.CustomerDocumentRepository;
-import com.arch.mfc.infra.outputadapter.nonrelational.MongoImpl;
+import com.arch.mfc.infra.inputadapter.QueryDocumentInputAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CustomerService extends MongoImpl<CustomerDocument> {
+public class CustomerQueryService extends QueryDocumentInputAdapter<CustomerDocument> {
 
     @Autowired
     CustomerDocumentRepository repository;
 
-    @Override
-    protected MongoRepository<CustomerDocument, String> getRepository() {
-        return repository;
+    public CustomerQueryService(CustomerDocumentRepository concreteRepository) {
+        super(concreteRepository);
     }
+
 }
