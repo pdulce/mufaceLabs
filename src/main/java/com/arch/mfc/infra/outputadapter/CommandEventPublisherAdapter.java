dffe759a873397110069410459109656616f3f53
pedro.dulce <pedro.dulce@gmail.com>
2024-05-30 20:29:05 +0200

    estable version

--- src/main/java/com/arch/mfc/infra/outputadapter/CommandEventPublisherAdapter.java
+++ src/main/java/com/arch/mfc/infra/outputadapter/CommandEventPublisherAdapter.java
@@ -2,6 +2,7 @@ package com.arch.mfc.infra.outputadapter;
 
 import com.arch.mfc.infra.event.Event;
 import com.arch.mfc.infra.outputport.CommandEventPublisher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -10,13 +11,14 @@ public class CommandEventPublisherAdapter implements CommandEventPublisher {
 
     private final KafkaTemplate<String, Event<?>> kafkaTemplate;
 
+    @Autowired
     public CommandEventPublisherAdapter(KafkaTemplate<String, Event<?>> kafkaTemplate) {
             this.kafkaTemplate = kafkaTemplate;
     }
 
     @Override
     public void publish(String topic, Event<?> eventArch) {
-        kafkaTemplate.send(topic.concat("-").concat(eventArch.getAlmacen()), eventArch.getId(), eventArch);
+        kafkaTemplate.send(topic, eventArch.getId(), eventArch);
     }
 
 }
