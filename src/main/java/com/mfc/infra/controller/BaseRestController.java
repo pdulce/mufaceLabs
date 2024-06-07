4f170a2acddaf7df986b80e2246d3e8a8276a9fa
pedro.dulce <pedro.dulce@gmail.com>
2024-06-07 19:53:52 +0200

    more cleaning code...depurado

--- src/main/java/com/mfc/infra/controller/BaseRestController.java
+++ src/main/java/com/mfc/infra/controller/BaseRestController.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
