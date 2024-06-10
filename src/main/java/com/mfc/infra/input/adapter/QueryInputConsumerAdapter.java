b5c6238fd7cb67790bb15065e25a037959d749ca
pedro.dulce <pedro.dulce@gmail.com>
2024-06-10 14:36:44 +0200

    more cleaning code...depurado

--- src/main/java/com/mfc/infra/input/adapter/QueryInputConsumerAdapter.java
+++ src/main/java/com/mfc/infra/input/adapter/QueryInputConsumerAdapter.java
@@ -8,6 +8,7 @@ import com.mfc.infra.utils.ConversionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.lang.reflect.ParameterizedType;
