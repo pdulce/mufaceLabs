ee0a56b03866e26475496d6702490dc6a60cff5e
pedro.dulce <pedro.dulce@gmail.com>
2024-06-10 21:14:21 +0200

    refactory fuerte

--- src/main/java/com/mfc/backend/microregalos/domain/repository/RegaloCommandRepository.java
+++ src/main/java/com/mfc/backend/microregalos/domain/repository/RegaloCommandRepository.java
@@ -1,7 +1,7 @@
 package com.mfc.backend.microregalos.domain.repository;
 
 import com.mfc.backend.microregalos.domain.model.Regalo;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.mfc.infra.output.port.GenericRepositoryPort;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
  */
 
 @Repository
-public interface RegaloCommandRepository extends JpaRepository<Regalo, Long> {
+public interface RegaloCommandRepository extends GenericRepositoryPort<Regalo, Long> {
 
 
 }
