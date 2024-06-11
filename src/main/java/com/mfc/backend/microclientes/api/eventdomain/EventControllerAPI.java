2ca3827d28a3e2f423928db6310aa4bff4060f9d
pedro.dulce <pedro.dulce@gmail.com>
2024-06-11 13:02:33 +0200

    con arquitectura de datos

--- src/main/java/com/mfc/backend/microclientes/api/eventdomain/EventControllerAPI.java
+++ src/main/java/com/mfc/backend/microclientes/api/eventdomain/EventControllerAPI.java
@@ -1,6 +1,6 @@
 package com.mfc.backend.microclientes.api.eventdomain;
 
-import com.mfc.backend.microclientes.domain.model.query.CustomerDocument;
+import com.mfc.backend.microclientes.domain.model.command.Customer;
 import com.mfc.infra.controller.BaseRestController;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +17,7 @@ public class EventControllerAPI extends BaseRestController {
 
     @GetMapping(value = "getAllFromEventStoreCustomers", produces=MediaType.APPLICATION_JSON_VALUE)
     public Map<String, List<Object>> getAllFromEventStoreCustomers() {
-        return this.eventStoreConsumerAdapter.findAll(CustomerDocument.class.getSimpleName());
+        return this.eventStoreConsumerAdapter.findAll(Customer.class.getSimpleName());
     }
 
     @GetMapping(value = "getTransactionsEvents", produces=MediaType.APPLICATION_JSON_VALUE)
@@ -27,12 +27,12 @@ public class EventControllerAPI extends BaseRestController {
 
     @GetMapping(value = "getAllEventsFromCustomerId", produces=MediaType.APPLICATION_JSON_VALUE)
     public List<Object> getAllEventsFromIdFromRedis(@RequestParam String customerId) {
-        return this.eventStoreConsumerAdapter.findById(CustomerDocument.class.getSimpleName(), customerId);
+        return this.eventStoreConsumerAdapter.findById(Customer.class.getSimpleName(), customerId);
     }
 
     @DeleteMapping(value = "deleteAlmacenCustomers", produces=MediaType.APPLICATION_JSON_VALUE)
     public void deleteAlmacenCustomers() {
-        this.eventStoreConsumerAdapter.deleteAll(CustomerDocument.class.getSimpleName());
+        this.eventStoreConsumerAdapter.deleteAll(Customer.class.getSimpleName());
     }
 
 
--- src/main/java/com/mfc/infra/output/adapter/CommandEventPublisherPortAdapter.java
+++ src/main/java/com/mfc/infra/output/adapter/CommandEventPublisherPortAdapter.java
@@ -1,6 +1,7 @@
 package com.mfc.infra.output.adapter;
 
 import com.mfc.infra.event.Event;
+import com.mfc.infra.output.port.CommandEventPublisherPort;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @ConditionalOnProperty(name = "arch.eventbroker.active", havingValue = "true", matchIfMissing = false)
-public class CommandEventPublisherPortAdapter {
+public class CommandEventPublisherPortAdapter implements CommandEventPublisherPort {
 
     Logger logger = LoggerFactory.getLogger(CommandEventPublisherPortAdapter.class);
     private final KafkaTemplate<String, Event<?>> kafkaTemplate;
@@ -20,5 +21,9 @@ public class CommandEventPublisherPortAdapter {
             this.kafkaTemplate = kafkaTemplate;
     }
 
+    @Override
+    public void publish(String topic, Event<?> eventArch) {
+        kafkaTemplate.send(topic, eventArch.getId(), eventArch);
+    }
 
 }
